4fcabca46883c67be98f5a153c4a5319da748be7
DavidJVitale <DavidJVitale@users.noreply.github.com>
2020-01-30 14:08:37 -0800

    Updated API ref with the dynamic redirect HTML/JS page to developers.arcgis.com

--- apidoc/update_all_html_pages_with_redirect.py
+++ apidoc/update_all_html_pages_with_redirect.py
@@ -1,4 +1,7 @@
+from glob import glob
 
+html_content = \
+"""
 <h1>This page has been moved!</h1>
 <h3>Redirecting you to <a href="https://developers.arcgis.com/python/api-reference/">developers.arcgis.com</a>.</h3>
 <p>If you are not automatically redirected, click <a id="dynamic-redirect-link" href="https://developers.arcgis.com/python/api-reference/">here</a>.</p>
@@ -14,3 +17,8 @@ if(el){
     el.href = newAddress;}
 window.location.href = newAddress; //this will automatically bring us to the new page
 </script>
+"""
+
+for path in glob("./html/**/*.html", recursive=True):
+    with open(path, "w") as f:
+        f.write(html_content)
